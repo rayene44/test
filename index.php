3a5468ec7a92967bf82608c02351d98235807297
rayene44 <khalsirayene12@gmail.com>
2025-01-26 14:47:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -292,13 +292,14 @@ $emailn=$_POST["emailn"];
 $date=$_POST["date"];
 $passwordn=$_POST["passwordn"];
 }
-$req="select from login where Email_com='email'";
+$req="select from login where Email_com='emailn'";
 $req=mysqli_query($cnx,$req);
 $n=mysqli_num_rows($req);
 if($n!=0)
 echo'<h1 id=h1ph>'."insertion échoué".'</h1>';
 else{
-    $req="insert into compte values('$nom','$prenom','$emailn','$date','$passwordn')";
+    $req="insert into compte values('$nom_com','$Prenom_com','$Email_com','$date_com');
     '<h1 id=h1ph>'."insertion avec succéé".'</h1>';
 }
+mysqli_close($cnx);
 ?>
