e727f1d74485e801c5827088d0e0d804247b7a54
rayene44 <khalsirayene12@gmail.com>
2025-01-26 13:55:53 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+function menu(){
+    document.getElementById("header").style.display="flex"
+}
+function returnp(){
+    document.getElementById("header").style.display="none"
+}
+function change(){
+    document.getElementById("box2").style.display="none"
+    document.getElementById("box3").style.display="flex"
+}
+function turn(){
+    document.getElementById("box2").style.display="flex"
+    document.getElementById("box3").style.display="none"
+}
