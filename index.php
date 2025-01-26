9220f2a04c01036a27a2b5ab0d2745b46aa034ba
rayene44 <khalsirayene12@gmail.com>
2025-01-26 15:07:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
 <?php
-$cnx=mysqli_connect("localhost","root","","login")or die("connection échoué");
-if(isset($nom)||isset($prenom)||isset($emailn)||isset($emailn)||isset($date)||isset($passwordn)){
-$nom=$_POST["nom"];
-$prenom=$_POST["prenom"];
-$emailn=$_POST["emailn"];
-$date=$_POST["date"];
-$passwordn=$_POST["passwordn"];
-}
-$req="select * from login where Email_com='emailn'";
-$res=mysqli_query($cnx,$req);
-$n=mysqli_num_rows($res);
-if($n!=0)
-echo"compte existant";
-else{
-    $req="insert into compte values('$nom','$Prenom','$passwordn','$emailn','$date');
-    $res2=mysqli-query($cnx,$req);
-    if($res2==true)
-    ech"insertion avec succéé";
-    else ech"insertion échoué";
-}
-mysqli_close($cnx);
+echo"hello"
 ?>
